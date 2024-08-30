1b048f38d47e0c2a2ca237c8b0fe559a0c8ae93f
Jeremy School <jschool@Jeremys-MacBook-Pro.local>
2024-08-30 16:22:40 -0400

    Enhanced script setup and sourcing.

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "alias ash='$(pwd)/run.sh'" >> ~/.zshrc
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-source local.sh && target/debug/ash
+script_path="$( cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd )"
+
+source $script_path/local.sh && $script_path/target/debug/ash
